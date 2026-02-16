1b1eadefd1229ebb558b5c6c98f6ca8aec13f42d
CiCi <96775840+weecici@users.noreply.github.com.>
2026-02-17 01:07:17 +0700

    test(frozen_lake): add test for q-learning agent

---
+++ test/frozen_lake/q_learning.py
@@ -0,0 +1,24 @@
+import gymnasium as gym
+import ale_py
+from j4f_rl.frozen_lake import *
+
+gym.register_envs(ale_py)
+env = gym.make(
+    "FrozenLake-v1", desc=None, map_name="8x8", is_slippery=False, render_mode="human"
+)
+
+
+if __name__ == "__main__":
+    observation, info = env.reset()
+    agent = QLearningAgent(env)
+    agent.learn()
+
+    for _ in range(1000):
+        action = agent.get_action(observation)
+
+        observation, reward, terminated, truncated, info = env.step(action)
+
+        if terminated or truncated:
+            observation, info = env.reset()
+
+    env.close()
