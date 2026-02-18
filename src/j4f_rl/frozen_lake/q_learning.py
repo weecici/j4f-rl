52769d5e8abe920b0a2c2c7ad5356e4f20189c36
CiCi <96775840+weecici@users.noreply.github.com.>
2026-02-18 22:07:18 +0700

    fix(frozen_lake): use linear decay for exploration eps

--- src/j4f_rl/frozen_lake/q_learning.py
+++ src/j4f_rl/frozen_lake/q_learning.py
@@ -11,7 +11,6 @@ class Agent:
     ):
         self.env = env
         self.q_table = np.zeros((env.observation_space.n, env.action_space.n))
-        # self.policy = np.zeros((env.observation_space.n), dtype=np.int8)
         self.alpha = alpha  # Learning rate
         self.gamma = gamma  # Discount factor
         self.eps = eps  # Convergence threshold
@@ -25,33 +24,48 @@ class Agent:
         self,
         num_episodes: int = 100,
         max_steps: int = 100,
-        exploration_eps: float = 1.0,
-        decay_eps: float = 0.99,
+        max_exp_eps: float = 1.0,
+        min_exp_eps: float = 0.05,
     ):
+
+        exploration_eps = max_exp_eps  # Exploration rate
+
+        # The delta for Linear decay of exploration rate
+        delta_exp_eps = (max_exp_eps - min_exp_eps) / num_episodes
+
         for episode in range(num_episodes):
             print(f"Q-Learning start episode {episode + 1}")
-            state, _ = self.env.reset()
+            curr_state, _ = self.env.reset()
             for _ in range(max_steps):
+
+                # Epsilon-greedy action selection
                 if random.random() < exploration_eps:
                     action = random.randint(0, self.env.action_space.n - 1)
                 else:
-                    action = self._get_greedy_action(state)
+                    action = self._get_greedy_action(curr_state)
 
+                # Take action and observe the next state and reward
                 next_state, reward, terminated, truncated, _ = self.env.step(action)
+
+                ### Update Q-table using the Q-learning update rule
+                # Q(s, a) = Q(s, a) + alpha * (reward + gamma * max(Q(s', a')) - Q(s, a))
                 best_future_q = self.q_table[next_state].max() * (terminated == 0)
                 temp_diff = (
-                    reward + self.gamma * best_future_q - self.q_table[state, action]
+                    reward
+                    + self.gamma * best_future_q
+                    - self.q_table[curr_state, action]
                 )
-
                 update_value = self.alpha * temp_diff
-                self.q_table[state, action] += update_value
+                self.q_table[curr_state, action] += update_value
+                ###
 
-                state = next_state
+                # Update current state
+                curr_state = next_state
 
                 if terminated or truncated:
                     break
 
-            exploration_eps *= decay_eps
+            exploration_eps -= delta_exp_eps
 
     def get_action(self, state):
         return self._get_greedy_action(state)
